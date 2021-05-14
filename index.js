c4acfcfcecb772c0d8adedd23640788cef8bf2df
Gustavo Manca <gustavo.manca@fatec.sp.gov.br>
2021-05-14 11:40:22 -0300

    refactor: call scrap method insite created server to response results

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 const http = require('http')
 
-const app = http.createServer(function (req, res) {
+const puppeteer = require('puppeteer')
+
+const URL = 'http://loterias.caixa.gov.br/wps/portal/loterias/landing'
+
+async function scrap() {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+  const page = await browser.newPage()
+  await page.goto(URL)
+
+  const data = await page.evaluate(() => {
+    const results = []
+    const items = document.querySelectorAll('.lotofacil td')
+    items.forEach(({ innerHTML }) => results.push(innerHTML))
+
+    const description = document.querySelector(
+      '.lotofacil ~ p.description'
+    ).innerText
+
+    return {
+      description,
+      results
+    }
+  })
+  await browser.close()
+
+  return data
+}
+
+const app = http.createServer(async function (_, res) {
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ a: 1 }))
+
+  const data = await scrap()
+
+  res.end(JSON.stringify(data))
 })
-app.listen(3000)
-
-// const puppeteer = require('puppeteer')
-
-// const URL = 'http://loterias.caixa.gov.br/wps/portal/loterias/landing'
-
-// async function execute() {
-//   const browser = await puppeteer.launch({
-//     args: ['--no-sandbox', '--disable-setuid-sandbox']
-//   })
-//   const page = await browser.newPage()
-//   await page.goto(URL)
-
-//   const data = await page.evaluate(() => {
-//     const results = []
-//     const items = document.querySelectorAll('.lotofacil td')
-//     items.forEach(({ innerHTML }) => results.push(innerHTML))
-
-//     const description = document.querySelector(
-//       '.lotofacil ~ p.description'
-//     ).innerText
-
-//     return {
-//       results,
-//       description
-//     }
-//   })
-
-//   console.log({ data })
-
-//   await browser.close()
-// }
-
-// execute()
